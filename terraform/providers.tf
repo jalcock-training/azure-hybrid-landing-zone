b4ca4be5f51d5187f9a6ce71a4c6158506a51ffc
James Alcock <alcock.james.training@proton.me>
2025-12-23 09:45:33 +1030

    added azapi provider

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,12 +2,16 @@
 # This file defines the required Terraform and AzureRM provider versions.
 
 terraform {
-  required_version = ">= 1.6.0"
+  required_version = ">= 1.7.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 4.0"
+    }
+    azapi = {
+      source  = "azure/azapi"
+      version = "~> 1.12"
     }
   }
 }
@@ -16,3 +20,5 @@ provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
 }
+
+provider "azapi" {}
