abd600b613fd7270a2460b6ed120fb48989a558d
James Alcock <alcock.james.training@proton.me>
2025-12-31 07:58:37 +1030

    Added shell login script

---
+++ scripts/jump-aci-shell.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+###############################################################################
+# jump-aci-shell.sh
+#
+# Creates a shell in the jump-aci container
+###############################################################################
+
+RESOURCE_GROUP="rg-platform"
+ACI_NAME="jump-aci"
+
+az container exec \
+    -g "$RESOURCE_GROUP" \
+    -n "$ACI_NAME" \
+    --exec-command "/bin/bash"
