787df6e6536ec17b55fd72948ef2aecfa6f6e90b
James Alcock <alcock.james.training@proton.me>
2026-01-02 07:27:57 +1030

    Cleanup workload vm internal name change

--- terraform/stage2-workloads/outputs.tf
+++ terraform/stage2-workloads/outputs.tf
@@ -3,10 +3,10 @@
 # ------------------------------------------------------------
 
 output "workload_vm_private_ip" {
-  value = module.workload_vm.vm_private_ip
+  value = module.workload_vm.workload_private_ip
 }
 
 output "workload_vm_identity_principal_id" {
-  value = module.workload_vm.vm_identity_principal_id
+  value = module.workload_vm.workload_identity_principal_id
 }
 
